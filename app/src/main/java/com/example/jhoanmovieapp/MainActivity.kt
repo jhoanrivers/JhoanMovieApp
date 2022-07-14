73f60597d98d2c5161b59d8c3c9cf9b7aec08f68
Jhoan River <jhoan.river@inmotion.co.id>
2022-07-15 00:16:17 +0700

    Add Configuration to get data from API - implemented Dagger Hilt - implemented Retrofit and OkHttp - Implemented View Model

--- app/src/main/java/com/example/jhoanmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/jhoanmovieapp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.jhoanmovieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.jhoanmovieapp.ui.viewmodel.MostPopularViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+
+    private val viewModel : MostPopularViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.getListMovie()
+
+
     }
 }
